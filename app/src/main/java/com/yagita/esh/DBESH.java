bd4d49430ec8cce266682b82d103625355481214
yagita2104 <yagita2104@gmail.com>
2023-11-04 01:05:23 +0700

    03/11/2023

--- app/src/main/java/com/yagita/esh/DBESH.java
+++ app/src/main/java/com/yagita/esh/DBESH.java
@@ -1,5 +1,6 @@
 package com.yagita.esh;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,8 +8,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.yagita.esh.json.JsonReader;
+import com.yagita.esh.json.JsonVocab;
+
+import org.json.JSONArray;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
 public class DBESH extends SQLiteOpenHelper {
     Context context;
+    List<JsonVocab> jsonVocabList;
     public DBESH(Context context) {
         super(context, "esh.sqlite", null, 1);
         this.context = context;
@@ -23,11 +35,41 @@ public class DBESH extends SQLiteOpenHelper {
         SQLiteDatabase database = getReadableDatabase();
         return database.rawQuery(sql, null);
     }
-
+    public void getData(){
+        JSONArray jsonArray = JsonReader.loadJSONArrayFromRaw(context, R.raw.data);
+        String data = "";
+        if(jsonArray != null){
+            data = jsonArray.toString();
+        }
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<JsonVocab>>() {}.getType();
+        jsonVocabList = gson.fromJson(data, listType);
+    }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String sql = "CREATE TABLE";
-        sqLiteDatabase.execSQL("");
+        String createTableQuery = "CREATE TABLE IF NOT EXISTS tblVocabulary (" +
+                "id TEXT PRIMARY KEY, " +
+                "english TEXT, " +
+                "sub_english TEXT, " +
+                "example TEXT, " +
+                "english_vocabulary_test TEXT, " +
+                "fill_blank TEXT, " +
+                "mistake TEXT, " +
+                "status INTEGER)";
+        sqLiteDatabase.execSQL(createTableQuery);
+        getData();
+        for (JsonVocab item : jsonVocabList) {
+            ContentValues values = new ContentValues();
+            values.put("id", item.getID());
+            values.put("english", item.getEnglish());
+            values.put("sub_english", item.getSub_English());
+            values.put("example", item.getExample());
+            values.put("english_vocabulary_test", new Gson().toJson(item.getEnglish_Vocabulary_Test()));
+            values.put("fill_blank", item.getFill_Blank());
+            values.put("mistake", new Gson().toJson(item.getMistake()));
+            values.put("status", item.getStatus());
+            sqLiteDatabase.insert("tblVocabulary", null, values);
+        }
     }
 
     @Override
